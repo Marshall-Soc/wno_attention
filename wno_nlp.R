1e77839607e6d367e572738066fe2032caa88950
Marshall A. Taylor <Marshall-Soc@users.noreply.github.com>
2023-11-30 12:26:13 -0700

    updates

--- wno_nlp.R
+++ wno_nlp.R
@@ -25,6 +25,10 @@ pacman::p_load(tidyverse,
                stm,
                install = T)
 
+remotes::install_gitlab("culturalcartography/text2map.dictionaries")
+
+library(text2map.dictionaries)
+
 
 ######################################
 #  Data
@@ -59,7 +63,7 @@ data <- data %>%
          text = trimws(text))
 
 pre.meta <- pre.meta[order(match(rownames(pre.meta), data$doc_id)),]
-# identical(rownames(pre.meta), data$doc_id)
+# identical(rownames(pre.meta), data$doc_id) hould be TRUE
 
 pre.meta$doc_id <- paste0("doc", 1:nrow(data))
 
@@ -76,7 +80,7 @@ lemmas <- udpipe_annotate(
   summarize(n = n())
 
   # Get triplet matrix
-df <- lemmas %>%
+df <- lemmas %>% #This can take a long time
   #remove words that are 2 characters long or less
   filter(!nchar(lemma) <= 2) %>%
   #remove English stop words in the Onix, SMART, or Snowball stop lists
@@ -101,19 +105,55 @@ dtm <- removeSparseTerms(dtm, 0.6)
 ######################################
 
   #Prep for STM
-pre.meta <- pre.meta[order(match(rownames(pre.meta), rownames(dtm))),]
-identical(rownames(pre.meta), rownames(dtm))
+pre.meta <- pre.meta[order(match(pre.meta$doc_id, rownames(dtm))),]
+# identical(pre.meta$doc_id, rownames(dtm)) should be TRUE
 
 out <- readCorpus(dtm, type = "slam")
 out <- prepDocuments(out$documents, out$vocab, pre.meta)
 docs <- out$documents
 vocab <- out$vocab
 meta <- out$meta 
+# identical(names(docs), meta$doc_id) Should be TRUE
+
+  # Find optimal k
+wn_modelk <- searchK(docs, vocab, K = seq(2, 20, by = 1), N = 50,
+                     heldout.seed = 0.5, init.type = "Spectral", 
+                     prevalence =~ s(year) + ideo,
+                     data = meta)
+
+plot(wn_modelk)
+ggplot(data = wn_modelk$results, aes(x = as.numeric(semcoh), y = as.numeric(exclus))) +
+  geom_text(aes(label = K)) +
+  xlab("Semantic Coherence") +
+  ylab("Exclusivity") +
+  theme_bw() +
+  theme(axis.title = element_text(face = "bold"),
+        panel.grid = element_blank()) #ideal statistical fit = 8-ish
+
+  # Get model
+wn.stm <- stm(docs, vocab, K = 8,
+              prevalence =~ s(year) + ideo,
+              data = meta,
+              init.type = "Spectral")
+
+  # Interpret the topics
+plot(wn.stm, type = "summary", n = 5, main = "", labeltype = "prob")
+
+sageLabels(wn.stm) #Topic #2 = Borders and Immigration
+
+data <- data[order(match(data$doc_id, rownames(pre.meta))),]
+# identical(data$doc_id, rownames(pre.meta)) should be TRUE
+
+findThoughts(wn.stm, texts = as.character(data$text),
+             n = 3, topics = 2) #Topic #2 = Borders and Immigration
+
+  # Add topic proportions to dataset
+data.final <- cbind(meta, wn.stm$theta)
 
 
-
-
-
+######################################
+#  Construal scores
+######################################
 
 
 
