c1189c6dd73216d62b7ab64f2ebfb15db3a59221
Marshall A. Taylor <mtaylor2@nmsu.edu>
2023-12-07 09:53:30 -0700

    updates

--- wno_attention.R
+++ wno_attention.R
@@ -95,10 +95,9 @@ freq_table(as.data.frame(doc_type), doc_type)
   #Note: Two other models are in the appendix section below: one with just
       #the environmental shock DV, and another that adds construal style
       #and the envr*construal interaction
-model <- plm(immigration ~ construal_style*terror_nr + discursive_style +
-               per_repub + vcrime_rate + factor(reform) +
-               factor(p2001) + log_vocality + word_count +
-               factor(admin),
+model <- plm(immigration ~ log_terror*log_hisppop + discursive_style +
+               per_repub + vcrime_rate + factor(reform) + p2001 + log_vocality + 
+               p2001 + factor(admin),
              data = wno_data,
              index = c("org","year"),
              model = "within")
@@ -112,23 +111,16 @@ mat <- perm_table(data = wno_data, model = model,
 #  Figure #3
 ######################################
 
-  #Note: This figure is the PPV plot for the full model (left panel) and 
-  #the model with 2002 org-years removed (right panel). The right panel is one
-  #of the two 9/11 bias robustness checks.
+mat$var <- factor(mat$rowname, levels = rev(mat$rowname))
 
-mat$var <- rownames(mat) %>%
-  as.factor()
-mat$var <- factor(mat$var, levels = rev(mat$var))
-
-labels <- c("Construal Style (sd)","Environmental Shock (logged)",
+labels <- c("U.S. Terror Threats/Attacks (logged)","Hispanic Population (logged)",
             "Discursive Style (logit-transformed)","% Republican Voting in County",
             "Violent Crime Rate in County (logged)","Immigration Legislation",
             "Pre-2001 Publication","Writer Heterogeneity (logged)",
             "(Mean) Word Count","Reagan Administration",
-            "H.W. Bush Administration","W. Bush Administration",
-            "Env Shock \u00D7 Construal")
+            "H.W. Bush Administration","W. Bush Administration")
 
-coefs <- mat$coef %>% round(3)
+coefs <- mat$stat %>% round(3)
 coefs.lab <- list()
   
 for (i in coefs) {
@@ -149,9 +141,10 @@ fig2 <- mat %>%
   theme_bw() +
   theme(axis.title = element_text(face = "bold"),
         axis.text.y = element_text(face = "bold"),
-        axis.text.x = element_text(size = 8)) +
+        axis.text.x = element_text(size = 8),
+        panel.grid.minor.x = element_blank()) +
   scale_y_continuous(expand = c(0.005,0),
-                     limits = c(0,1),
+                     limits = c(0,1.2),
                      breaks = c(0,.05,seq(.25, 1, by = .25)),
                      labels = c("0.00", expression(paste(italic("p"),
                                                          " = 0.05")),
@@ -167,51 +160,74 @@ dev.off()
 #  Figure #4
 ######################################
 
-#Note: This figure is the PPV plot for the full model (left panel) and 
-#the model with 2002 org-years removed (right panel). The right panel is one
-#of the two 9/11 bias robustness checks.
+    #Note: This figure is the prediction plot for the full model (left panel) and 
+    #the model with 2002 org-years removed (right panel). The right panel is one
+    #of the two 9/11 bias robustness checks.
 
 wno_data2 <- wno_data[wno_data$year != 2002,] #Removing 2002 org-years
 
-model2 <- plm(lo_immigrant_dup ~ stdlagconabst_pol2_dup*terror_nr + laglo_immigrant_dup +
-                per_repub1_dup + lo_vcrime_rate + factor(reform) +
-                factor(p2001) + logvocality_dup + word_count_dup +
-                factor(admin42),
+model2 <- plm(immigration ~ log_terror*log_hisppop + discursive_style +
+                per_repub + vcrime_rate + factor(reform) + p2001 + log_vocality + 
+                p2001 + factor(admin),
               data = wno_data2,
-              index = c("org2","year"),
+              index = c("org","year"),
               model = "within") #the model with 2002 org-years removed
 
 mat2 <- perm_table(data = wno_data2, model = model2,
-                  perm_v = "lo_immigrant_dup", statistic = "coefficients",
-                  strata_v = "org2", seed = 123)
+                  perm_v = "immigration", statistic = "coefficients",
+                  strata_v = "org", seed = 123)
 
 labs <- c("2002 Org-Years Included","2002 Org-Years Not Included")
 names(labs) <- c("model","model2")
 
-fig3 <- predictions(model, variables = list(stdlagconabst_pol2_dup = seq(-1,1, by  = 1),
-                                         terror_nr = seq(-3,1, by = .5))) %>%
+model.data <- as.data.frame(model$model)
+
+model.data <- model.data %>%
+  rename(admin = `factor.admin.`,
+         reform = `factor.reform.`)
+
+model.data2 <- as.data.frame(model2$model)
+
+model.data2 <- model.data2 %>%
+  rename(admin = `factor.admin.`,
+         reform = `factor.reform.`)
+
+fig3 <- predictions(model, variables = list(log_terror = seq(quantile(wno_data$log_terror, .25, na.rm = T), 
+                                                             quantile(wno_data$log_terror, .75, na.rm = T),
+                                                             by = .25),
+                                            log_hisppop = c(quantile(wno_data$log_hisppop, .25, na.rm = T),
+                                                            quantile(wno_data$log_hisppop, .5, na.rm = T),
+                                                            quantile(wno_data$log_hisppop, .75, na.rm = T))),
+                    newdata = model.data) %>%
   rbind(.,
-        predictions(model2, variables = list(stdlagconabst_pol2_dup = seq(-1,1, by  = 1),
-                                            terror_nr = seq(-3,1, by = .5)))) %>%
-  mutate(model_id = c(rep("model", 2970), rep("model2", (tally(.) - 2970)))) %>%
-  select(rowid, predicted, stdlagconabst_pol2_dup, terror_nr, model_id) %>%
+        predictions(model2, variables = list(log_terror = seq(quantile(wno_data$log_terror, .25, na.rm = T), 
+                                                             quantile(wno_data$log_terror, .75, na.rm = T),
+                                                             by = .25),
+                                            log_hisppop = c(quantile(wno_data$log_hisppop, .25, na.rm = T),
+                                                            quantile(wno_data$log_hisppop, .5, na.rm = T),
+                                                            quantile(wno_data$log_hisppop, .75, na.rm = T))),
+                    newdata = model.data2)) %>%
+  mutate(model_id = c(rep("model", 1296), rep("model2", (tally(.) - 1296))),
+         log_terror = (exp(log_terror)-1),
+         log_hisppop = exp(log_hisppop)) %>%
+  select(rowid, predicted, log_hisppop, log_terror, model_id) %>%
   mutate(predicted = (exp(predicted)/(1 + exp(predicted)))) %>%
-  group_by(model_id, stdlagconabst_pol2_dup, terror_nr) %>%
+  group_by(log_hisppop, log_terror, model_id) %>%
   summarise(predicted_sum = mean(predicted)) %>%
-  ggplot(aes(x = terror_nr, y = predicted_sum, linetype = as.factor(stdlagconabst_pol2_dup))) +
+  ggplot(aes(x = log_terror, y = predicted_sum, linetype = as.factor(log_hisppop))) +
   geom_line() +
   labs(y = '"Borders and Immigration" Grievance Probability',
-       x = "Ratio of Terror Events per 1,000 Non-Hispanic/Latinx County Residents (logged)") +
-  scale_linetype_discrete(breaks = c(-1,0,1),
-                        labels = c("-1 SD Construal Style",
-                                   "Mean Construal Style",
-                                   "+1 SD Construal Style"),
-                        name = "") +
+       x = "Number of Non-Right Wing Terror Threats or Attacks in U.S. in Previous Year") +
+  scale_linetype_discrete(#breaks = c(-1,0,1),
+    labels = c(expression("25"^{th}~"Percentile Hispanic Population in County"),
+               "Median Hispanic Population in County",
+               expression("75"^{th}~"Percentile Hispanic Population in County")),
+    name = "") +
   theme_bw() +
   theme(axis.title = element_text(face = "bold"),
         legend.position = "top") +
   facet_grid(~model_id,
-             labeller = labeller(model_id = labs))
+           labeller = labeller(model_id = labs))
 
 png("figures/fig3.png", res = 750, height = 6, width = 12, units = "in")
   fig3
@@ -223,42 +239,56 @@ dev.off()
 ######################################
 
   #Note: This is the other 9/11 robustness check, where, instead of removing
-      #2002 org-years, I made a env*construal*post-2001 three-way interaction.
+      #2002 org-years, I made a terro*hisp*post-2001 three-way interaction.
 
-model3 <- plm(lo_immigrant_dup ~ stdlagconabst_pol2_dup*terror_nr*factor(p2001) + 
-               laglo_immigrant_dup + per_repub1_dup + lo_vcrime_rate + factor(reform) +
-               logvocality_dup + word_count_dup +
-               factor(admin42),
+model3 <- plm(immigration ~ log_terror*log_hisppop*p2001 + discursive_style +
+               per_repub + vcrime_rate + factor(reform) + log_vocality + 
+               p2001 + factor(admin),
              data = wno_data,
-             index = c("org2","year"),
+             index = c("org","year"),
              model = "within")
 
-mat3 <- perm_table(data = wno_data, model = model3,
-                  perm_v = "lo_immigrant_dup", statistic = "coefficients", 
-                  strata_v = "org2", seed = 123)
+mat3 <- perm_table(data = wno_data, model = model,
+                  perm_v = "immigration", statistic = "coefficients", 
+                  strata_v = "org", seed = 123)
+
+model.data3 <- as.data.frame(model3$model)
+
+model.data3 <- model.data3 %>%
+  rename(admin = `factor.admin.`,
+         reform = `factor.reform.`)
 
 labs2 <- c("Pre-9/11","Post-9/11")
 names(labs2) <- c("0","1")
 
-fig7 <- predictions(model3, variables = list(stdlagconabst_pol2_dup = seq(-1,1, by  = 1),
-                                    terror_nr = seq(-3,1, by = .5),
-                                    p2001 = c(0,1))) %>%
-  select(rowid, predicted, stdlagconabst_pol2_dup, terror_nr, p2001) %>%
+fig7 <- predictions(model3, variables = list(log_terror = seq(quantile(wno_data$log_terror, .25, na.rm = T), 
+                                                             quantile(wno_data$log_terror, .75, na.rm = T),
+                                                             by = .25),
+                                            log_hisppop = c(quantile(wno_data$log_hisppop, .25, na.rm = T),
+                                                            quantile(wno_data$log_hisppop, .5, na.rm = T),
+                                                            quantile(wno_data$log_hisppop, .75, na.rm = T)),
+                                            p2001 = c(0,1)),
+                    newdata = model.data3) %>%
+  mutate(log_terror = (exp(log_terror)-1),
+         log_hisppop = exp(log_hisppop),
+         p2001 = as.factor(p2001)) %>%
+  select(rowid, predicted, log_hisppop, log_terror, p2001) %>%
   mutate(predicted = (exp(predicted)/(1 + exp(predicted)))) %>%
-  group_by(p2001, stdlagconabst_pol2_dup, terror_nr) %>%
+  group_by(log_hisppop, log_terror, p2001) %>%
   summarise(predicted_sum = mean(predicted)) %>%
-  ggplot(aes(x = terror_nr, y = predicted_sum, 
-             linetype = as.factor(stdlagconabst_pol2_dup),
-             group = interaction(p2001, as.factor(stdlagconabst_pol2_dup)),
+  ggplot(aes(x = log_terror, y = predicted_sum, 
+             linetype = as.factor(log_hisppop),
+             group = interaction(p2001, as.factor(log_hisppop)),
              color = p2001)) +
   geom_line() +
   labs(y = '"Borders and Immigration" Grievance Probability',
-       x = "Ratio of Terror Events per 1,000 Non-Hispanic/Latinx County Residents (logged)",
+       x = "Number of Non-Right Wing Terror Threats or Attacks in U.S. in Previous Year",
        color = "", linetype = "") +
-  scale_linetype_discrete(breaks = c(-1,0,1),
-                        labels = c("-1 SD Construal Style",
-                                     "Mean Construal Style",
-                                     "+1 SD Construal Style")) +
+  scale_linetype_discrete(#breaks = c(-1,0,1),
+    labels = c(expression("25"^{th}~"Percentile Hispanic Population in County"),
+               "Median Hispanic Population in County",
+               expression("75"^{th}~"Percentile Hispanic Population in County")),
+    name = "") +
   scale_color_manual(breaks = c(0,1),
                      values = c("#1696d2","#fdbf11"),
                      labels = c("Before/Including 2001","Post-2001")) +
@@ -278,8 +308,8 @@ dev.off()
 ######################################
 
 #Variance decomposition model
-vd <- lmer(lo_immigrant_dup ~ 1 + (1|org2),
-           data = wno_data)
+vd <- lmer(immigration ~ 1 + (1|org),
+           data = wno_data[complete.cases(wno_data),])
 summary(vd)
 
 vd.re <- VarCorr(vd) %>% as.data.frame()
@@ -288,16 +318,18 @@ vd.re$vcov[2]/(vd.re$vcov[1] + vd.re$vcov[2]) #1 - ICC
 
 #Unconditional growth model
 ugm <- wno_data %>%
-  group_by(org_shortened) %>%
-  mutate(imm_y = lo_immigrant_dup - mean(lo_immigrant_dup)) %>%
+  filter(complete.cases(.)) %>%
+  group_by(org) %>%
+  mutate(imm_y = immigration - mean(immigration)) %>%
   ungroup() %>%
   ggplot(aes(x = year, y = imm_y)) +
   geom_point(shape = 1) +    
   geom_smooth(method = "lm", se = FALSE, color = "black") +
   xlab("Year") + ylab(expression(paste("(Imm Focus)"[ij] - bar("(Imm Focus)")[j]))) +
-  facet_wrap(~org_shortened) +
+  facet_wrap(~org, labeller = label_wrap_gen(width = 50)) +
   xlim(1980, 2008) +
-  theme_bw() 
+  theme_bw() +
+  theme(strip.text = element_text(size = 6))
 
 png("figures/ugm.png", res = 750, height = 6, width = 8, units = "in")
   ugm
@@ -306,9 +338,9 @@ dev.off()
 
 #Descriptive statistics
   #For continuous variables
-vars.con <- c("lo_immigrant_dup","stdlagconabst_pol2_dup","terror_nr",
-               "laglo_immigrant_dup","per_repub1_dup","lo_vcrime_rate",
-               "logvocality_dup","word_count_dup")
+vars.con <- c("immigration","log_terror","log_hisppop",
+               "discursive_style","per_repub","vcrime_rate",
+               "log_vocality","word_count")
 describe(wno_data[vars.con])
 
   #For categorical variables, dummy
@@ -316,41 +348,40 @@ vars.cat <- c("reform","p2001")
 apply(wno_data[vars.cat], 2, mean)
 
   #For categorical variables, multiple categories
-freq_table(wno_data, admin42)
+freq_table(wno_data, admin)
 
 #Models 1 and 2 in Table 5 of the Appendix
   #Model 1
-model.1 <- plm(lo_immigrant_dup ~ terror_nr,
+model.1 <- plm(immigration ~ log_terror,
                data = wno_data,
-               index = c("org2","year"),
+               index = c("org","year"),
                model = "within")
 
 mat.1 <- perm_table(data = wno_data, model = model.1,
-                  perm_v = "lo_immigrant_dup", statistic = "coefficients", 
-                  strata_v = "org2", seed = 123)
+                  perm_v = "immigration", statistic = "coefficients", 
+                  strata_v = "org", seed = 123)
 
   #Model 2
-model.2 <- plm(lo_immigrant_dup ~ stdlagconabst_pol2_dup*terror_nr,
+model.2 <- plm(immigration ~ log_terror*log_hisppop,
                data = wno_data,
-               index = c("org2","year"),
+               index = c("org","year"),
                model = "within")
 
 mat.2 <- perm_table(data = wno_data, model = model.2,
-                    perm_v = "lo_immigrant_dup", statistic = "coefficients",
-                    strata_v = "org2", seed = 123)
+                    perm_v = "immigration", statistic = "coefficients",
+                    strata_v = "org", seed = 123)
 
   #Fit statistics for all models
 fit <- list(
-  feols(lo_immigrant_dup ~ terror_nr | org2, data = wno_data),
-  feols(lo_immigrant_dup ~ stdlagconabst_pol2_dup*terror_nr | org2, data = wno_data),
-  feols(lo_immigrant_dup ~ stdlagconabst_pol2_dup*terror_nr + laglo_immigrant_dup +
-          per_repub1_dup + lo_vcrime_rate + factor(reform) +
-          factor(p2001) + logvocality_dup + word_count_dup +
-          factor(admin42) | org2, data = wno_data)
+  feols(immigration ~ log_terror | org, data = wno_data),
+  feols(immigration ~ log_terror*log_hisppop | org, data = wno_data),
+  feols(immigration ~ log_terror*log_hisppop + discursive_style +
+          per_repub + vcrime_rate + factor(reform) + p2001 + log_vocality + 
+          p2001 + factor(admin) | org, data = wno_data)
 )
 
 lapply(fit, r2) #r2, adj r2, within-r2, adj within-r2
-lapply(fit, function(x) sd(wno_data$lo_immigrant_dup - x$fitted.values)) #rmse
+lapply(fit, function(x) sd(wno_data$immigration - x$fitted.values)) #rmse
 
 fit.2 <- list(
   feols(lo_immigrant_dup ~ stdlagconabst_pol2_dup*terror_nr + laglo_immigrant_dup +
